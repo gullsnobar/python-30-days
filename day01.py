7579048247bc4fd3eb5e82982ab2adaffadfcf77
Gull Snobar <gullsnobar07@gmail.com>
2026-03-22 23:16:36 +0500

    Day01 of python learning journey, learn print and identation

---
+++ day01.py
@@ -0,0 +1,7 @@
+print("Hello, World!");
+
+age = 18
+
+if age >= 18:
+    print("You are an adult")   # ✅ This line is indented, belongs to the if
+print("Check complete")         # ✅ This line is NOT indented, outside the if
