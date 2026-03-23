881f0b2ba98882cee29a568af7d0775fc81ef88a
Gull Snobar <gullsnobar07@gmail.com>
2026-03-23 23:16:06 +0500

    python have not syntax to add multiline comments

--- day01.py
+++ day01.py
@@ -17,3 +17,33 @@ print("Check complete")
 
 print("Have a good day");
 print("Python is fun and easy to learn");
+
+
+# By default, the print() function ends with a new line
+# If you want to print multiple words on the same line, you can use the end parameter:
+
+print("Hello Gull", end = " ");
+print("How are you?")
+
+
+
+# We can also use print to print the numbers
+
+
+print( 2 + 2 );
+print( 2 - 2)
+print(8 * 3)
+
+print("Hello I am John, I am", 25,  "years old");
+
+
+# Multiline Comments
+# Python does not really have a syntax for multiline comments.
+
+# To add a multiline comment you could insert a # for each line:
+
+# Example
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
