acf0b67aa2f42ee85d12dd59ecc4019142a687e4
Gull Snobar <gullsnobar07@gmail.com>
2026-03-24 22:02:59 +0500

    In day02 I learned about variables and casting

---
+++ day02.py
@@ -0,0 +1,61 @@
+#  Python Varibales
+#  Variables are containers to store values
+
+x = 5;
+y = "John"
+
+print(x);
+print(y);
+
+#  Casting
+#  If you want to specify the datatype of a variable, then can be done by casting.
+
+x = str(5);
+y = int(8);
+z = float(6);
+
+# To check output print x,y and z
+print(x);
+print(y);
+print(z);
+
+
+#  Get the type
+
+x = 10;
+y = "Snobar Amjad"
+
+print(type(x))
+print(type(y))
+
+
+# Single or Double Quotes?
+# String variables can be declared either by using single or double quotes
+
+
+x = "Snob";
+x = 'Snob'
+
+#  Case Sebsitive
+#  In python Variables names are case sensitive
+
+a = 8;
+a = "Tayyab";
+
+
+# Multi Words Variable Names
+# Variable names with more than one word can be difficult to read.
+
+# There are several techniques you can use to make them more readable:
+
+# Camel Case
+# Each word, except the first, starts with a capital letter:
+# myVariableName = "John"
+
+# Pascal Case
+# Each word starts with a capital letter:
+# MyVariableName = "John"
+
+# Snake Case
+# Each word is separated by an underscore character:
+# my_variable_name = "John"
