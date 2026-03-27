709ece160f5d4001ce55c30589292ceba910debe
gullsnobar <abdulrehmanali7775@gmail.com>
2026-03-27 21:12:49 +0500

    Functions with Arguments are done!

---
+++ day03.py
@@ -0,0 +1,21 @@
+# Functions in Python
+
+#  A function is a reuseable block of code that performs a specific task
+#  Instead of writing a block of code again and again you write that code inside function and reuse it.
+
+def greet():
+    print("How are you?")
+greet();
+
+#  Another example
+
+def say_hello():
+    print("Hello World!");
+say_hello()
+
+# Functions with Parameters
+# You can pass data into a function
+
+def greet(name):
+    print("Helle", name);
+greet("Gull");
