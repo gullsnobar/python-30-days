96ddcac587eda1a140ccef412bf0bb8bc4c5e077
gullsnobar <abdulrehmanali7775@gmail.com>
2026-03-29 20:40:04 +0500

    Strings and its methods are done

--- day04.py
+++ day04.py
@@ -5,3 +5,32 @@ def check_even(num):
         return ("Odd")
 
 print(check_even(7));
+
+
+#  Strings in Python
+
+# Multiline strings
+
+a = """ Hello World """
+print(a)
+
+# Upper Case: The upper case method returns the strings in upper case.
+
+a = "Snobar"
+print(a.upper());
+
+# Lower Case: The lower case method returns the strings in lower case. 
+
+a = "Snobar"
+print(a.lower())
+
+# Remove Whitespace
+# Strip method removes the white spaces from the start or in the end.
+
+a = " Tayyab "
+print(a.strip())
+
+# Replace String
+
+a =  "Martoon"
+print(a.replace("M", "C"))
