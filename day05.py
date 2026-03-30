745166315ea1fb5ae4ae57d294d6e3b1c0676a50
gullsnobar <abdulrehmanali7775@gmail.com>
2026-03-30 13:35:21 +0500

    python boolean code challenge is done

--- day05.py
+++ day05.py
@@ -20,3 +20,17 @@ y = b;
 
 print(bool(x));
 print(bool(y));
+
+
+x1 = 0;
+print(bool(x1))
+
+
+# Most Values are True
+# Almost any value is evaluated to True if it has some sort of content.
+
+# Any string is True, except empty strings.
+
+# Any number is True, except 0.
+
+# Any list, tuple, set, and dictionary are True, except empty ones.
