302b29dca5b32541a99450df00a5e7f24e72fdbc
gullsnobar <abdulrehmanali7775@gmail.com>
2026-03-30 13:28:42 +0500

    Python booleans are done

---
+++ day05.py
@@ -0,0 +1,22 @@
+# Python Booleans
+
+# Booleans represent one of two values: True or False.
+
+a = 200;
+b = 45;
+
+if(a > b):{
+    print("a is greater than b")
+}
+else: {
+    print("a is not greater than b")
+}
+
+# Evaluate Values and Variables
+# The bool() function allows you to evaluate any value, and give you True or False in return,
+
+x = "Snobar";
+y = b;
+
+print(bool(x));
+print(bool(y));
