2d21d36384f0646670d72b5ac3ddb782b2680957
gullsnobar <abdulrehmanali7775@gmail.com>
2026-04-04 22:40:48 +0500

    lists done

---
+++ day07.py
@@ -0,0 +1,11 @@
+# Python Lists
+
+# Lists are used to store multiple items in a single variable.
+
+mylist = ["apple", "orange" ,"banana" ,"graphes"]
+print(mylist);
+
+# Lists Items
+# Lists items are ordered, changeable and allow duplicate values.
+
+
