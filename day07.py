e3383392e362e4410c99f13320c01ae66c5ffd79
gullsnobar <abdulrehmanali7775@gmail.com>
2026-04-04 22:47:51 +0500

    list Constractor are done

--- day07.py
+++ day07.py
@@ -9,3 +9,15 @@ print(mylist);
 # Lists items are ordered, changeable and allow duplicate values.
 
 
+# List Length
+# To determine how many items a list has, use the len() function:
+
+list = ["Hello", "Snobar", "Amjad", "Ali"]
+print(len(list));
+
+
+# The list() Constructor
+# It is also possible to use the list() constructor when creating a new list.
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
