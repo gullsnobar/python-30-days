b8185fea4cd9ad7e3b41db4dd0d2c35c901d8043
gullsnobar <abdulrehmanali7775@gmail.com>
2026-04-05 12:28:57 +0500

    lists, set and tuples are done

--- day07.py
+++ day07.py
@@ -1,5 +1,4 @@
 # Python Lists
-
 # Lists are used to store multiple items in a single variable.
 
 mylist = ["apple", "orange" ,"banana" ,"graphes"]
@@ -16,8 +15,60 @@ list = ["Hello", "Snobar", "Amjad", "Ali"]
 print(len(list));
 
 
-# The list() Constructor
-# It is also possible to use the list() constructor when creating a new list.
 
-thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
-print(thislist)
+# Example of lists
+
+fruits = ["apple", "mango", "orange", "grapes"]
+print(fruits);
+
+# Example 2 (Modify lists)
+
+numbers = [1,2,3,4,5];
+numbers[0] = 10;
+print(numbers)
+
+
+# What is a Tuple?
+
+# A tuple is like a list BUT:
+
+# Ordered 
+# NOT changeable (immutable) 
+# Allows duplicates 
+
+# Think: “fixed data (cannot change)”
+
+
+# Example 1
+
+colors = ("red", "green", "blue", "yellow");
+print(colors);
+
+# Example 2
+
+# colors = ("red", "green", "blue", "yellow");
+# colors[1] = "purple"    # here it shows error becoz tuple is immutable
+# print(colors)
+
+
+
+# What is a Set?
+
+# A set is:
+
+# Unordered 
+# No duplicates 
+# Changeable 
+
+# Think: “unique values only”
+
+
+numbers = {1,2,3,3,4,5,6,6,7,8,9,10}
+print(numbers)
+
+# Add Items is set allows
+
+my_lists = {"apple", "banana", "orange"}
+my_lists.add("cherry");
+print(my_lists);
+        
