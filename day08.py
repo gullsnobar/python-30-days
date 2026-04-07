9d873d889f1a6c642396f11ec47e79b3e3cb6713
gullsnobar <abdulrehmanali7775@gmail.com>
2026-04-07 06:59:12 +0500

    if-elif-else statement complete

--- day08.py
+++ day08.py
@@ -1,4 +1,5 @@
 #  Python If Else statement
+# if else statent used when you want two outcomes.
 
 a = 200;
 b = 40;
@@ -28,3 +29,26 @@ if age >= 18:
   print("You are an adult")
   print("You can vote")
   print("You have full legal rights")
+
+
+age = 16;
+if age >= 18:
+   print("You can vote");
+else:
+   print("You cannot vote");
+
+
+# if-elif-else (Multiple Conditions)
+# Used when you have multiple choices
+
+marks = 70;
+
+if marks >= 85:
+   print("You are Topper");
+elif marks >= 75:
+   print("You are good student");
+elif marks >= 65:
+   print("You are average student");
+else:
+   print("You need to work hard")
+
