0f1871869a79e4ebfc5918574c43c47ea2e74dd4
gullsnobar <abdulrehmanali7775@gmail.com>
2026-04-09 23:29:02 +0500

    loops are done

--- day09.py
+++ day09.py
@@ -19,3 +19,38 @@ match day:
     print("Saturday")
   case 7:
     print("Sunday")
+
+
+
+# Loops: A loop repeat a block of code multiple times
+
+for i in range(5):
+  print("Hello, I am Gull.")
+
+
+for i in range(10):
+  print("Hello, I am Gull.")
+
+
+
+
+# 2. Types of Loops in Python
+# There are 2 main loops:
+
+# 1. for loop → used when you know how many times to run
+# 2. while loop → used when condition decides
+
+# Example 1: Print numbers 1 to 5
+
+for i in range(1, 6):
+  print(i)
+
+for j in range(1, 20):
+  print(j)
+
+# Example 2: Loop through a list
+
+names = ["Ali", "Sana", "Salman", "Ahmad"]
+for name in names:
+  print(name)
+
