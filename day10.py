0968b9e8212183f8a0167c834b1b70300bb65161
gullsnobar <abdulrehmanali7775@gmail.com>
2026-04-10 19:35:18 +0500

    Did examples of loops

--- day10.py
+++ day10.py
@@ -1,37 +1,58 @@
 # While loop
 
 # Basic Counter
-# i = 1
+i = 1
 
-# while i <= 5:
-#     print(i);
-#     i += 1
+while i <= 5:
+    print(i)
+    i += 1
 
 
-# Infinite Loop
+# Infinite Loop (⚠️ Uncomment to run)
 # while True:
 #     print("Hello")
 
+
 # Control Statement
 # Break loop
-
-
 for i in range(10):
-        if i == 5:
-            break
-        print (i)
+    if i == 5:
+        break
+    print(i)
 
-        # Continue
-        # Skip current iteration
 
-        for j in range(5):
-              if j == 2:
-                    continue
-              print(j);
+# Continue
+# Skip current iteration
+for j in range(5):
+    if j == 2:
+        continue
+    print(j)
 
-#  pass statement do nothing
 
+# pass statement (does nothing)
 print("Pass statement does nothing")
 
 for i in range(7):
-      pass
+    pass
+
+
+# Example: Even and Odd Numbers
+numbers = [40, 37, 89, 28, 20, 46, 90, 39]
+
+for num in numbers:
+    if num % 2 == 0:
+        print(num, "is even")
+    else:
+        print(num, "is odd")
+
+
+# Another example
+# Find Maximum Number in the list
+numbers = [3, 6, 9, 5, 89, 67, 2]
+max_number = numbers[0]
+
+for num in numbers:
+    if num > max_number:
+        max_number = num
+
+print("Max:", max_number)
