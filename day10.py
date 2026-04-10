fdc9b7e3ed7db89299e5baa3e32f7cdf714b522b
gullsnobar <abdulrehmanali7775@gmail.com>
2026-04-10 19:17:04 +0500

    Done

---
+++ day10.py
@@ -0,0 +1,37 @@
+# While loop
+
+# Basic Counter
+# i = 1
+
+# while i <= 5:
+#     print(i);
+#     i += 1
+
+
+# Infinite Loop
+# while True:
+#     print("Hello")
+
+# Control Statement
+# Break loop
+
+
+for i in range(10):
+        if i == 5:
+            break
+        print (i)
+
+        # Continue
+        # Skip current iteration
+
+        for j in range(5):
+              if j == 2:
+                    continue
+              print(j);
+
+#  pass statement do nothing
+
+print("Pass statement does nothing")
+
+for i in range(7):
+      pass
