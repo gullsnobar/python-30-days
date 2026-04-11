2fb26db4823bcf050dcef82d4430ca2dc86f7c22
gullsnobar <abdulrehmanali7775@gmail.com>
2026-04-11 23:43:12 +0500

    Arrays are started

---
+++ day11.py
@@ -0,0 +1,8 @@
+# Array = A container that stores multiple values in one variable
+
+# But important point 👇
+# ⚠️ Python does NOT have built-in arrays like C/Java
+# 👉 Instead, we mostly use Lists as arrays
+
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
