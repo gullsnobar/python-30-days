b9df6e09100b1d61b94f17010680b5a58dcde788
gullsnobar <abdulrehmanali7775@gmail.com>
2026-04-14 23:49:45 +0500

    function example

---
+++ day12.py
@@ -0,0 +1,17 @@
+# Python Function
+# Function is a block of code which only runs when it is called
+# A function helps avoiding code repeatition
+
+def my_function():
+    print("Hello, World!")
+my_function()
+
+
+
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
