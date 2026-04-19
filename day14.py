e3897791d773400de726a9f1db617d6e0eab3edf
gullsnobar <abdulrehmanali7775@gmail.com>
2026-04-19 18:28:34 +0500

    Exception Handling

--- day14.py
+++ day14.py
@@ -20,3 +20,26 @@
 
 # The finally block lets you execute code, regardless of the result of the try- and except blocks.
 
+# Basic Example (Handling Errors)
+
+try:
+    x = int(input("Enter a number: "))
+    print(10 / x)
+except:
+    print("Something went wrong!")
+    
+    
+    # Specific Exception Handling
+    
+    try:
+        x = int(input("Enter a Number: "))
+        result = 10 / x;
+    except ValueError:
+        print("Invalid Input! Please enter a number.")    
+    except ZeroDivisionError:
+        print("You cannot divide by zero.")
+    
+    
+    
+    
+
